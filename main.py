89f1f165488d17b76505ecfe720e65fbae9d72cb
Sida Chen <git@sidac.me>
2018-10-30 11:28:42 -0400

    Make parser configurable

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+import json
 from bill_of_materials import BillOfMaterials
 from formatter import Formatter
-import parsers.npm, parsers.pip
 
 bill_of_materials_path = "bill_of_materials.json"
 formatter_path = "formatter.config.json"
+parser_config = "parsers.config.json"
+
+
+def load_parser_config(path):
+    ps = []
+    with open(path) as f:
+        config = json.load(f)
+        npm_config = config.get("npm")
+        if npm_config:
+            import parsers.npm
+            ps.append(parsers.npm.get(**npm_config))
+
+        pip_config = config.get("pip")
+        if pip_config:
+            import parsers.pip
+            ps.append(parsers.pip.get(**pip_config))
+
+    return ps
+
 
 if __name__ == '__main__':
     formatter = Formatter(formatter_path)
     bill = BillOfMaterials(bill_of_materials_path, formatter)
-    retrievers = [parsers.npm.get, parsers.pip.get]
+    retrievers = load_parser_config(parser_config)
 
     for f in retrievers:
         for material in f():
             bill.add(material)
-    
+
     bill.save()
--- parsers/npm.py
+++ parsers/npm.py
@@ -2,20 +2,24 @@ import json, os
 from subprocess import check_output
 from bill_of_materials import Material
 
-npm_licence_checker_binary = "/usr/local/bin/license-checker"
-package_json_including_folder = "/Users/sidchen/Dev/quay/"
-npm_command = "cd %s && %s --json --excludePrivatePackages" % (
-    package_json_including_folder, npm_licence_checker_binary)
 npm_debug_file = "npm-debug.json"
 format_javascript = "Javascript"
 
 
-def get():
-    output = check_output(npm_command, shell=True, env=os.environ)
-    with open(npm_debug_file, "w+") as f:
-        f.write(output)
+def get(package_json_including_folder, npm_licence_checker_binary):
+    npm_command = "cd %s && %s --json --excludePrivatePackages" % (
+        package_json_including_folder, npm_licence_checker_binary)
 
-    jsDict = json.loads(output)
-    print "npm: found %d packages" % (len(jsDict), )
-    for name, details in jsDict.iteritems():
-        yield Material(''.join(name.split("@")[:-1]), details["licenses"], format_javascript, details.get("repository", details.get("url", "")))
+    def gen():
+        output = check_output(npm_command, shell=True, env=os.environ)
+        with open(npm_debug_file, "w+") as f:
+            f.write(output)
+
+        jsDict = json.loads(output)
+        print "npm: found %d packages" % (len(jsDict), )
+        for name, details in jsDict.iteritems():
+            yield Material(''.join(name.split("@")[:-1]), details["licenses"],
+                           format_javascript,
+                           details.get("repository", details.get("url", "")))
+
+    return gen
